8d8e6c83097ec707ec8676e074f7f611a6568d41
nicktheo59 <zcapfc0@live.ucl.ac.uk>
2013-12-24 03:00:15 +0000

    vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treeboo::Application.config.secret_key_base = '1994ba148971a3b8a220cc1c939c0b25eb00c3040b8dce4ba9843b71f6d85491b72ab3d738789de30c22d7b582bbdcb74f91bb36b9fa288ba4c00d3125fe2c17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Treeboo::Application.config.session_store :cookie_store, key: '_treeboo_session'
